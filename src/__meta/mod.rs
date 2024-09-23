6c266fe8b7b3382130a024d2109dcede579ff23e
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-09-23 07:30:14 +0200

    initial commit

---
+++ src/__meta/mod.rs
@@ -0,0 +1,12 @@
+pub mod digital_input;
+pub mod digital_output;
+
+pub mod bpc;
+pub mod relay;
+pub mod blc;
+pub mod thermometer;
+pub mod powermeter;
+pub mod serial;
+pub mod registers;
+pub mod voltmeter;
+pub mod ammeter;
