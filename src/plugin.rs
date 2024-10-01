b04e616b7348c20adf2a932003c69d8d5b896ffe
Xavier RODRIGUEZ <xavier.rodriguezalvarez@gmail.com>
2024-10-01 15:50:34 +0200

    up

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,5 +1,7 @@
 use std::ffi::CStr;
 
+pub struct Builder {}
+
 ///
 /// This structure provides the plugin interface
 ///
@@ -12,18 +14,32 @@ pub struct Plugin {
     pub name: *const i8,
     pub version: *const i8,
     pub test: extern "C" fn(),
-    pub join: extern "C" fn(), // get_producers -> function to get producers
-                               // produce -> function to create a new device
-                               //
+    pub join: extern "C" fn(),
+
+    ///
+    /// Produce a device matching the given json string configuration
+    pub produce: extern "C" fn(*const i8),
+    //
+    //
+    // get_producers -> function to get producers
+    // produce -> function to create a new device
+    //
 }
 
 impl Plugin {
-    pub fn new(name: &CStr, version: &CStr, test: extern "C" fn(), join: extern "C" fn()) -> Self {
+    pub fn new(
+        name: &'static CStr,
+        version: &CStr,
+        test: extern "C" fn(),
+        join: extern "C" fn(),
+        produce: extern "C" fn(*const i8),
+    ) -> Self {
         Plugin {
             name: name.as_ptr() as *const i8,
             version: version.as_ptr() as *const i8,
             test: test,
             join: join,
+            produce: produce,
         }
     }
 }
