acab92f19f9313e3bedaa61e4eaa95be4cf82499
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-11-12 22:12:02 +0100

    up

--- src/settings/si.rs
+++ src/settings/si.rs
@@ -1,4 +1,28 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value as JsonValue;
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
 ///
 ///
 ///
-struct EnumSi {}
+struct EnumSi {
+    unit: String,
+    min: i32,
+    max: i32,
+}
+
+impl EnumSi {
+    pub fn new<N: Into<String>>(unit: N, min: i32, max: i32) -> Self {
+        Self {
+            unit: unit.into(),
+            min,
+            max,
+        }
+    }
+}
+
+impl Into<JsonValue> for EnumSi {
+    fn into(self) -> JsonValue {
+        serde_json::to_value(self).unwrap()
+    }
+}
