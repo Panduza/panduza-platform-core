03f5bd09815846c1a69432dea2582a54adf91bff
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-11-12 22:03:00 +0100

    up

--- src/settings/eenum.rs
+++ src/settings/eenum.rs
@@ -5,12 +5,18 @@ use serde_json::Value as JsonValue;
 ///
 ///
 ///
-struct EnumSettings {
+pub struct EnumSettings {
     choices: Vec<String>,
+}
+
+impl From<Vec<String>> for EnumSettings {
+    fn from(values: Vec<String>) -> Self {
+        Self { choices: values }
+    }
 }
 
 impl Into<JsonValue> for EnumSettings {
     fn into(self) -> JsonValue {
-        todo!()
+        serde_json::to_value(self.choices).unwrap()
     }
 }
