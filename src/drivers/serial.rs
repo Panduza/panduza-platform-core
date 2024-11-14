aea82a04a0d067662688c328cdff9729d8d5e945
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-11-14 16:41:53 +0100

    trait serial driver

--- src/drivers/serial.rs
+++ src/drivers/serial.rs
@@ -2,5 +2,19 @@ pub mod eol;
 pub mod generic;
 pub mod settings;
 pub mod time_lock;
+use async_trait::async_trait;
 
 pub use settings::Settings;
+
+use crate::Error;
+
+#[async_trait]
+pub trait SerialDriver {
+    async fn write(&mut self, command: &[u8]) -> Result<usize, Error>;
+
+    async fn write_then_read(
+        &mut self,
+        command: &[u8],
+        response: &mut [u8],
+    ) -> Result<usize, Error>;
+}
--- src/drivers/serial/eol.rs
+++ src/drivers/serial/eol.rs
@@ -9,6 +9,9 @@ use crate::format_driver_error;
 use crate::log_debug;
 use crate::log_info;
 
+use super::SerialDriver;
+use async_trait::async_trait;
+
 ///
 ///
 pub struct Driver {
@@ -31,8 +34,11 @@ impl Driver {
             eol: eol,
         })
     }
+}
 
-    pub async fn write(&mut self, command: &[u8]) -> Result<usize, Error> {
+#[async_trait]
+impl SerialDriver for Driver {
+    async fn write(&mut self, command: &[u8]) -> Result<usize, Error> {
         //
         // Append EOL to the command
         let mut internal_cmd = command.to_vec();
@@ -46,7 +52,7 @@ impl Driver {
 
     /// Lock the connector to write a command then wait for the answers
     ///
-    pub async fn write_then_read(
+    async fn write_then_read(
         &mut self,
         command: &[u8],
         response: &mut [u8],
@@ -64,24 +70,4 @@ impl Driver {
 
         return Ok(count - self.eol.len());
     }
-
-    // pub async fn get_idn(&mut self) -> Result<String, Error> {
-    //     let mut response: [u8; 512] = [0; 512];
-
-    //     // let cmd = "*IDN?\n".as_bytes();
-    //     let cmd = "*IDN?".as_bytes();
-
-    //     let count = self
-    //         .driver
-    //         .write_then_read_until(cmd, &mut response, '\n' as u8)
-    //         .await?;
-
-    //     println!("{:?}", response[..count].to_vec());
-
-    //     // count -1 because we remove the '\n'
-    //     let string_slice = String::from_utf8(response[..count - 1].to_vec()).unwrap();
-    //     let string = string_slice.to_string();
-
-    //     Ok(string)
-    // }
 }
